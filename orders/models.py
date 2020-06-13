97bda0614a573411efdaec85b4042007391efaaf
Parth <parthjpatelx@gmail.com>
2020-06-13 19:45:14 -0400

    all files

---
+++ orders/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+# Pizza class
+    #type= Pizzas class 
+    #toppings_int = integer <= 3 
+    #size = Sizes class 
+    #price = decimal
+
+#Pizzas class
+    #style = charfield
+
+#Sizes class
+    #size = charfield
+
+#Toppings class
+    #name = charfiled
+
+
+#Pizza(type = sicilian, toppings_int =  2, size = small, price = 3.30)
+
+#Sub class
+    #type = Subs class
+    #size = Sizes class
+    #toppings = optional, Manytomanyrelationship? 
+    #price = decimal 
+
+
+#Subs class
+    #name = charfield
+
+#TOODO: Write models for Pasta, Salads and Dinner platters after testing pizza/sub classes
+

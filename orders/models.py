481222a205d9e0c26933fcb958241e011ea83e7a
Parth <parthjpatelx@gmail.com>
2020-06-15 17:42:22 -0400

    added additional models

--- orders/models.py
+++ orders/models.py
@@ -2,33 +2,50 @@ from django.db import models
 
 # Create your models here.
 
-# Pizza class
-    #type= Pizzas class 
-    #toppings_int = integer <= 3 
-    #size = Sizes class 
-    #price = decimal
+# Create your models here.
+class Pizzas(models.Model):
+    style = models.CharField(max_length=64)
 
-#Pizzas class
-    #style = charfield
+    def __str(self):
+        return f'{self.style}'
 
-#Sizes class
-    #size = charfield
+class Sizes(models.Model):
+    size = models.CharField(max_length=64)
 
-#Toppings class
-    #name = charfiled
+class Pizza(models.Model):
+    type = models.ForeignKey(Pizzas, on_delete=models.CASCADE)
+    # set toppings to 0 if toppings are not specified
+    toppings_int = models.IntegerField()
+    size = models.ForeignKey(Sizes, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=4, decimal_places=4)
 
+    def __str__(self):
+        return f'Pizza type: {self.type}, Toppings {self.toppings_int}, Size = {self.size}, Price = {self.price}' 
 
-#Pizza(type = sicilian, toppings_int =  2, size = small, price = 3.30)
+class Toppings(models.Model):
+    toppings = models.CharField(max_length=64)
 
-#Sub class
-    #type = Subs class
-    #size = Sizes class
-    #toppings = optional, Manytomanyrelationship? 
-    #price = decimal 
+class Subs(models.Model):
+    type = models.CharField(max_length=64)
 
+class Sub(models.Model):
+    type = models.ForeignKey(Subs, on_delete=models.CASCADE)
+    size = models.ForeignKey(Sizes, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=4, decimal_places=4)
+    toppings = models.ManyToManyField(Toppings, blank=True)
 
-#Subs class
-    #name = charfield
+class Pasta(models.Model):
+    name = models.ForeignKey(Pastas, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=4, decimal_places=4)
+    protein = models.ForeignKey(PastaProtein, on_delete=models.CASCADE)
 
-#TOODO: Write models for Pasta, Salads and Dinner platters after testing pizza/sub classes
+class PastaProtein(models.Model):
+    name = models.CharField(max_length=64)
 
+class Platter(models.Model):
+    name = models.ForeignKey(Platters, on_delete=models.CASCADE)
+    size = models.ForeignKey(Sizes, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=4, decimal_places=4)
+
+class Platters(models.Model):
+    type = models.CharField(max_length=64)

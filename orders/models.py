47c161534dff02ac4d2b0136fb000ef5d363acfb
Parth <parthjpatelx@gmail.com>
2020-06-15 17:48:19 -0400

    added str methods

--- orders/models.py
+++ orders/models.py
@@ -25,8 +25,14 @@ class Pizza(models.Model):
 class Toppings(models.Model):
     toppings = models.CharField(max_length=64)
 
+    def __str(self):
+        return f'{self.toppings}'
+
 class Subs(models.Model):
     type = models.CharField(max_length=64)
+
+    def __str(self):
+        return f'{self.type}'
 
 class Sub(models.Model):
     type = models.ForeignKey(Subs, on_delete=models.CASCADE)
@@ -34,18 +40,33 @@ class Sub(models.Model):
     price = models.DecimalField(max_digits=4, decimal_places=4)
     toppings = models.ManyToManyField(Toppings, blank=True)
 
+    def __str(self):
+        return f'Type: {self.type}, Size = {self.size}, {self.price}, {self.toppings}'
+
 class Pasta(models.Model):
     name = models.ForeignKey(Pastas, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=4, decimal_places=4)
     protein = models.ForeignKey(PastaProtein, on_delete=models.CASCADE)
 
+    def __str(self):
+        return f'Name: {self.name}, Price: {self.price}, Protein: {self.protein}'
+
 class PastaProtein(models.Model):
     name = models.CharField(max_length=64)
+
+    def __str(self):
+        return f'Name: {self.name}'
 
 class Platter(models.Model):
     name = models.ForeignKey(Platters, on_delete=models.CASCADE)
     size = models.ForeignKey(Sizes, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=4, decimal_places=4)
 
+    def __str(self):
+        return f'Name: {self.name}, Size: {self.size}, Price: {self.price}'
+
 class Platters(models.Model):
     type = models.CharField(max_length=64)
+
+    def __str(self):
+        return f'Type: {self.type}'

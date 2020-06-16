0de6211933916e1f8c86f7a42c11e1f09d393504
Parth <parthjpatelx@gmail.com>
2020-06-15 20:21:09 -0400

    added menu items, pending subs

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from orders.models import Pizzas, Sizes, Pizza, Toppings, Sub, Subs, Pasta,Pastas,PastaProtein, Salad, Platter, Platters
+
+admin.site.register(Pizza)
+admin.site.register(Pizzas)
+admin.site.register(Sizes)
+admin.site.register(Toppings)
+admin.site.register(Sub)
+admin.site.register(Subs)
+admin.site.register(Pasta)
+admin.site.register(Pastas)
+admin.site.register(PastaProtein)
+admin.site.register(Salad)
+admin.site.register(Platter)
+admin.site.register(Platters)
+
+
+
+
 
 # Register your models here.
--- orders/models.py
+++ orders/models.py
@@ -1,35 +1,39 @@
 from django.db import models
 
-# Create your models here.
-
-# Create your models here.
+#Added
 class Pizzas(models.Model):
     style = models.CharField(max_length=64)
 
     def __str__(self):
         return f'{self.style}'
 
+# added - of note, delete 'small' duplicate entry
 class Sizes(models.Model):
-    size = models.CharField(max_length=64)
+    size = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
         return f'{self.size}'
 
+# cheese = 0 toppings
+# special = 4 toppings
+
 class Pizza(models.Model):
     type = models.ForeignKey(Pizzas, on_delete=models.CASCADE)
     # set toppings to 0 if toppings are not specified
-    toppings_int = models.IntegerField()
+    toppings = models.IntegerField()
     size = models.ForeignKey(Sizes, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=4, decimal_places=4)
+    price = models.FloatField(max_length=4)
 
     def __str__(self):
-        return f'Pizza type: {self.type}, Toppings {self.toppings_int}, Size = {self.size}, Price = {self.price}' 
+        return f'Pizza type: {self.type}, Toppings {self.toppings}, Size = {self.size}, Price = {self.price}' 
 
 class Toppings(models.Model):
     toppings = models.CharField(max_length=64)
 
     def __str__(self):
         return f'{self.toppings}'
+
+#how to add extra cheese to a sub? Add an optional boolean?
 
 class Subs(models.Model):
     type = models.CharField(max_length=64)
@@ -40,42 +44,52 @@ class Subs(models.Model):
 class Sub(models.Model):
     type = models.ForeignKey(Subs, on_delete=models.CASCADE)
     size = models.ForeignKey(Sizes, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=4, decimal_places=4)
-    toppings = models.ManyToManyField(Toppings, blank=True)
+    price = models.FloatField(max_length=4)
+    # toppings = models.ManyToManyField(Toppings, blank=True)
 
     def __str__(self):
-        return f'Type: {self.type}, Size = {self.size}, {self.price}, {self.toppings}'
+        return f'Type: {self.type}, Size = {self.size}, {self.price}'
+
+    
+class Pastas(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class PastaProtein(models.Model):
+    protein = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.protein}'
 
 class Pasta(models.Model):
     name = models.ForeignKey(Pastas, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=4, decimal_places=4)
+    price = price = models.FloatField(max_length=4)
     protein = models.ForeignKey(PastaProtein, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Name: {self.name}, Price: {self.price}, Protein: {self.protein}'
 
-class Pastas(models.Model):
-    name = models.CharField(max_length=64)
-
-    def __str__(self):
-        return f'name: {self.name}'
-
-class PastaProtein(models.Model):
-    name = models.CharField(max_length=64)
-
-    def __str__(self):
-        return f'Name: {self.name}'
-
-class Platter(models.Model):
-    name = models.ForeignKey(Platters, on_delete=models.CASCADE)
-    size = models.ForeignKey(Sizes, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=4, decimal_places=4)
-
-    def __str__(self):
-        return f'Name: {self.name}, Size: {self.size}, Price: {self.price}'
 
 class Platters(models.Model):
     type = models.CharField(max_length=64)
 
     def __str__(self):
-        return f'Type: {self.type}'
+        return f'{self.type}'
+
+class Platter(models.Model):
+    name = models.ForeignKey(Platters, on_delete=models.CASCADE)
+    size = models.ForeignKey(Sizes, on_delete=models.CASCADE)
+    price = models.FloatField(max_length=4)
+
+    def __str__(self):
+        return f'Name: {self.name}, Size: {self.size}, Price: {self.price}'
+
+class Salad(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.FloatField(max_length=4)
+
+    def __str__(self):
+        return f'Name: {self.name}, Price: {self.price}'
+

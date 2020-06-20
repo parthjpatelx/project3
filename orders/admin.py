5629046ace2127cdf28aae318d7dc0de6432c388
parthjpatelx <53063794+parthjpatelx@users.noreply.github.com>
2020-06-20 15:56:51 -0400

    Add files via upload

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from orders.models import Pizza_style, Size, Pizza, Topping, Sub, Sub_type, Pasta,Pasta_type,PastaProtein, Salad, Platter, Platter_type, Cart
 
-# Register your models here.
+admin.site.register(Pizza)
+admin.site.register(Pizza_style)
+admin.site.register(Size)
+admin.site.register(Topping)
+admin.site.register(Sub)
+admin.site.register(Sub_type)
+admin.site.register(Pasta)
+admin.site.register(Pasta_type)
+admin.site.register(PastaProtein)
+admin.site.register(Salad)
+admin.site.register(Platter)
+admin.site.register(Platter_type)
+admin.site.register(Cart)
+
